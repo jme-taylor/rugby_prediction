90b68cfa905297ee1de68f278036d7d9bcc3de1b
jme-taylor <jamiecharlestaylor94@gmail.com>
2022-12-07 17:43:06 +0000

    add load raw data function

---
+++ rugby_prediction/data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from rugby_prediction.constants import DATA_FOLDER
+
+
+def load_raw_data() -> pd.DataFrame:
+    """
+    Function that loads the raw data csv from the data folder and returns
+    this as a pandas dataframe
+
+    Returns
+    -------
+    raw_match_data: pd.DataFrame
+        A pandas dataframe containing the raw match data - as ingested from
+        source.
+    """
+    raw_match_data_path = DATA_FOLDER.joinpath('match_data.csv')
+    raw_match_data = pd.read_csv(raw_match_data_path)
+    return raw_match_data
